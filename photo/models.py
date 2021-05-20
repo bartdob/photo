db504a05e5139469114d7e64313040ee4cd546d7
Bartosz Dobrosielski <dobry1@pm.me>
2021-05-20 13:14:52 +0200

    add login and logut template

--- photo/models.py
+++ photo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Photo(models.Model):
     title = models.CharField(max_length=80)
