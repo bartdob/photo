12c1b57d5bf9a1e74ed52cb1c1e124a33eb5d23f
Bartosz Dobrosielski <dobry1@pm.me>
2021-05-20 12:16:11 +0200

    photo model

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=80)
+    image = models.ImageField(default='photo/default.jpg', upload_to='photo')
+    date = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
