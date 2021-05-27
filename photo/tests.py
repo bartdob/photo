da467d35f5cdbd17d5ce69590b6facf1f01c815c
Bartosz Dobrosielski <dobry1@pm.me>
2021-05-27 14:24:25 +0200

    add tests

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
